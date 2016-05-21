7edfb611f8bf896fa61411d9118f366776fea9c6
Piotr Kasprzyk <ciri@ciri.pl>
2016-05-21 16:21:10 +0200

    Renamed to: said

--- db/storage.py
+++ db/storage.py
@@ -3,7 +3,7 @@
 
 def fetch_history():
     return [
-        {"id": 12, "user_id": 12312412, "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "told": "no", },
-        {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312, "told": "no", },
-        {"id": 16, "user_id": 12312419, "description": "O Światowe Dni Młodzieży", "ts": 121312313, "told": "no", },
+        {"id": 12, "user_id": 12312412, "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", },
+        {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", },
+        {"id": 16, "user_id": 12312419, "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "no", },
     ]
