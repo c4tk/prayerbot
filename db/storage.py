49715d638908530c8a82bd6f51d64f14b0529137
Kuba Odias <kuba.odias@gmail.com>
2016-05-21 21:42:50 +0200

    Use correct user IDs.

--- db/storage.py
+++ db/storage.py
@@ -3,11 +3,11 @@
 
 def fetch_history():
     data = [
-        {"id": 12, "user_id": "12312412", "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", "commiter_id": "12412414", },
-        {"id": 13, "user_id": "12312414", "description": "O powrót do zdrowia", "ts": 121312313, "said": "yes", "commiter_id": "131241241", },
-        {"id": 15, "user_id": "12312418", "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", "commiter_id": None, },
-        {"id": 16, "user_id": "12312419", "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", "commiter_id": "131241241", },
-        {"id": 17, "user_id": "12312419", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "yes", "commiter_id": "131241241", },
-        {"id": 18, "user_id": "12312420", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "no", "commiter_id": None, },
+        {"id": 12, "user_id": "1099770976753951", "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", "commiter_id": "1099770976753951", },
+        {"id": 13, "user_id": "1099770976753951", "description": "O powrót do zdrowia", "ts": 121312313, "said": "yes", "commiter_id": "", },
+        {"id": 15, "user_id": "215380638847054", "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", "commiter_id": None, },
+        {"id": 16, "user_id": "10208414992228182", "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
+        {"id": 17, "user_id": "10208414992228182", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
+        {"id": 18, "user_id": "215380638847054", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "no", "commiter_id": None, },
     ]
     return data
