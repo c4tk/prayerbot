ae10c52ae62f4e7884b73c48ca9f1bbfa5d79fc7
Kuba Odias <kuba.odias@gmail.com>
2016-05-21 17:52:54 +0200

    Shuffle and limit returned DB data

--- db/storage.py
+++ db/storage.py
@@ -7,5 +7,7 @@ def fetch_history():
         {"id": 13, "user_id": 12312414, "description": "O powrót do zdrowia", "ts": 121312313, "said": "yes", "commiter_id": 131241241, },
         {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", "commiter_id": None, },
         {"id": 16, "user_id": 12312419, "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", "commiter_id": 131241241, },
+        {"id": 17, "user_id": 12312419, "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "yes", "commiter_id": 131241241, },
+        {"id": 18, "user_id": 12312420, "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "no", "commiter_id": None, },
     ]
     return data
