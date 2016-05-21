99a8ec6b5d890e114cd97bbfdecc7667d84ed8b3
Piotr Kasprzyk <ciri@ciri.pl>
2016-05-22 00:08:49 +0200

    Move data

--- db/storage.py
+++ db/storage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 
+data = [
+    {"id": 12, "user_id": "1099770976753951", "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", "commiter_id": "1099770976753951", },
+    {"id": 13, "user_id": "1099770976753951", "description": "O powrót do zdrowia", "ts": 121312313, "said": "yes", "commiter_id": "", },
+    {"id": 15, "user_id": "215380638847054", "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", "commiter_id": None, },
+    {"id": 16, "user_id": "10208414992228182", "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
+    {"id": 17, "user_id": "10208414992228182", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
+    {"id": 18, "user_id": "215380638847054", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "no", "commiter_id": None, },
+]
 def fetch_history():
-    data = [
-        {"id": 12, "user_id": "1099770976753951", "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", "commiter_id": "1099770976753951", },
-        {"id": 13, "user_id": "1099770976753951", "description": "O powrót do zdrowia", "ts": 121312313, "said": "yes", "commiter_id": "", },
-        {"id": 15, "user_id": "215380638847054", "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", "commiter_id": None, },
-        {"id": 16, "user_id": "10208414992228182", "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
-        {"id": 17, "user_id": "10208414992228182", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "yes", "commiter_id": "1099770976753951", },
-        {"id": 18, "user_id": "215380638847054", "description": "W intencji Bogu wiadomej", "ts": 121312313, "said": "no", "commiter_id": None, },
-    ]
     return data
