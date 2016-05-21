e837bd4a18ac8cc499741e0a3c58867a31e1d6aa
Piotr Kasprzyk <ciri@ciri.pl>
2016-05-21 15:58:31 +0200

    Added comma

--- db/storage.py
+++ db/storage.py
@@ -4,5 +4,5 @@
 def fetch_history():
     return [
         {"id": 12, "user_id": 12312412, "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331},
-        {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312}
+        {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312},
     ]
