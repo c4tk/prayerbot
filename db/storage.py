959498fe4e8ef1e93f087f1a5c637e9113db8574
Piotr Kasprzyk <ciri@ciri.pl>
2016-05-21 16:23:09 +0200

    Said yes

--- db/storage.py
+++ db/storage.py
@@ -5,5 +5,5 @@ def fetch_history():
     return [
         {"id": 12, "user_id": 12312412, "description": "Potrzebuję modlitwy w intencji mojej mamy", "ts": 1412412331, "said": "no", },
         {"id": 15, "user_id": 12312418, "description": "O rozeznanie drogi", "ts": 121312312, "said": "no", },
-        {"id": 16, "user_id": 12312419, "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "no", },
+        {"id": 16, "user_id": 12312419, "description": "O Światowe Dni Młodzieży", "ts": 121312313, "said": "yes", },
     ]
