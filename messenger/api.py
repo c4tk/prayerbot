61c93e25d49cfd3af415879ba75fcdafe212dd8e
Kuba Odias <kuba.odias@gmail.com>
2016-05-21 14:04:37 +0200

    Fix connection closing.

--- messenger/api.py
+++ messenger/api.py
@@ -10,19 +10,20 @@ GRAPH_API_URL = "/v2.6/me"
 class MessengerApi:
     def __init__(self):
         self.access_token = os.environ.get('ACCESS_TOKEN')
-        if self.access_token:
-            self.conn = httplib.HTTPSConnection(GRAPH_API, 443)
-        else:
+        if self.access_token == None:
             print("Environment variable ACCESS_TOKEN is not set")
             sys.exit(2)
         
     def request(self, path, body = None):
+        # TODO: make connection persistent and init in __init__
+        conn = httplib.HTTPSConnection(GRAPH_API, 443)
         url = GRAPH_API_URL + path + "?access_token=" + self.access_token
         print("* HTTP request: POST " + GRAPH_API + url)
-        self.conn.request("POST", url, body, {"Content-type": "application/json"})
-        response = self.conn.getresponse()
+        conn.request("POST", url, body, {"Content-type": "application/json"})
+        response = conn.getresponse()
         print("* HTTP response: " + str(response.status) + " " + response.reason)
         response_data = response.read()
         if response_data != '':
             print("  body: " + response_data)
+        conn.close()
 
