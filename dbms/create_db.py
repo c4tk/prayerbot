7d4e22501eca3c75709e0c6fbba7a5c014615eb1
samuel.urbanowicz <samiuelson@gmail.com>
2016-10-23 12:11:54 +0200

    Delete mocked prayer requests

--- dbms/create_db.py
+++ dbms/create_db.py
@@ -6,12 +6,12 @@ from models import Intent, BibleVerse
 db.create_all()
 
 # insert prayer requests
-db.session.add(Intent(1099770976753951, u"Potrzebuje modlitwy w intencji mojej mamy"))
-db.session.add(Intent(1099770976753951, u"O powrot do zdrowia"))
-db.session.add(Intent(1209178385783730, u"O rozeznanie drogi"))
-db.session.add(Intent(10208414992228182, u"O Swiatowe Dni Mlodziezy"))
-db.session.add(Intent(10208414992228182, u"W intencji Bogu wiadomej"))
-db.session.add(Intent(215380638847054, u"W intencji Bogu wiadomej"))
+# db.session.add(Intent(1099770976753951, u"Potrzebuje modlitwy w intencji mojej mamy"))
+# db.session.add(Intent(1099770976753951, u"O powrot do zdrowia"))
+# db.session.add(Intent(1209178385783730, u"O rozeznanie drogi"))
+# db.session.add(Intent(10208414992228182, u"O Swiatowe Dni Mlodziezy"))
+# db.session.add(Intent(10208414992228182, u"W intencji Bogu wiadomej"))
+# db.session.add(Intent(215380638847054, u"W intencji Bogu wiadomej"))
 
 # insert Bible verses
 db.session.add(BibleVerse(u"Lecz ci, co zaufali Panu, odzyskują siły, otrzymują skrzydła jak orły: biegną bez zmęczenia, bez znużenia idą", u"Iz40,31"))
